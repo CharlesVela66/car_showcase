bb5fec16b94ef26fb9f2b46d86a2c68b869d1de8
Carlos <A01708634@tec.mx>
2023-08-01 13:02:44 -0600

    Navbar and Footer

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -11,7 +11,7 @@ const Hero = () => {
 
   return (
     <div className='hero'>
-      <div className='flex-1 pt-36 p-x'>
+      <div className='flex-1 pt-36 padding-x'>
         <h1 className='hero__title'>
           Find, book or rent a car -- quickly and easily!
         </h1>
@@ -25,6 +25,13 @@ const Hero = () => {
           containerStyles="bg-primary-blue text-white rounded-full mt-10"
           handleClick={handleScroll}
         />
+      </div>
+      <div className='hero__image-container'>
+        <div className='hero__image'>
+          <Image src='/hero.png' alt='hero' fill className='object-contain'/>
+        </div>
+
+        <div className='hero__image-overlay' />
       </div>
     </div>
   )
